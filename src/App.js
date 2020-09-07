c6e1ca38a14ac9ba0e2659aff22d8f28a1d21041
viorel2000@hotmail.com <$ git config --global user.email viorel2000@hotmail.com>
2020-09-07 12:00:05 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ASD <code>_I_</code> YOU!
         </p>
         <a
           className="App-link"
